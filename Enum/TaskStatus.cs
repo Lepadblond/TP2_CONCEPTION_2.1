137a060a137dc3d9c0b85e9a9b0a4ec297075c87
Lepadblond <girard.djs@gmail.com>
2024-11-13 15:22:39 -0500

    commit initial

---
+++ Enum/TaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Automate.Enum
+{
+    /// <summary>
+    /// Enum spécifiant le statut de la tâche
+    /// </summary>
+    public enum TaskStatus
+    {
+        [Description("À faire")]
+        AFaire = 0,
+        [Description("En cours")]
+        EnCours = 1,
+        [Description("Complété")]
+        Complete = 2
+    }
+}
